74436a7d5ee25a0acb44a1a7f9f3ebfdc4635b5d
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2023-06-10 00:07:25 +0530

    updates

--- php-src/scholar-api.php
+++ php-src/scholar-api.php
@@ -5,7 +5,7 @@ require '../api/google-search-results-php/restclient.php';
 
 $query = [
  "engine" => "google_scholar",
- "q" => "Assessment of the suitability of fog computing in the context of internet of things",
+ "q" => "10.1109/GLOBECOM48099.2022.10001379",
 ];
 
 $search = new GoogleSearch('8a5bbef9212e41dbcfb43b49f89214809a1851a38e9cc18d47054ba722f838eb');
@@ -13,8 +13,9 @@ $result = $search->get_json($query);
 $n_res = $result->organic_results;
 
 $res = json_encode(
-    array($n_res[0])
+    array($n_res)
 );
 
 print_r($res);
 
+// ->organic_results[0]->publication_info->authors
